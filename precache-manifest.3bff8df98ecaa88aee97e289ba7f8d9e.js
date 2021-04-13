67c3c4b6968b41b7d6d3e0675f951a483f281834
Darya Lutuva <lutowa.darya@yandex.ru>
2021-04-13 11:34:37 +0300

    Updates

---
+++ precache-manifest.3bff8df98ecaa88aee97e289ba7f8d9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4c8a00697141a0cedc7",
+    "url": "/registration-form/static/css/main.bc9e4371.chunk.css"
+  },
+  {
+    "revision": "c4c8a00697141a0cedc7",
+    "url": "/registration-form/static/js/main.2f859d7c.chunk.js"
+  },
+  {
+    "revision": "5bdef9117639bd7cd0b3",
+    "url": "/registration-form/static/js/runtime~main.29cf85e7.js"
+  },
+  {
+    "revision": "712ef76a6ae921aecbe3",
+    "url": "/registration-form/static/js/2.1dc631bd.chunk.js"
+  },
+  {
+    "revision": "839f5a3a81f0a7ea9f3a",
+    "url": "/registration-form/static/js/3.97381408.chunk.js"
+  },
+  {
+    "revision": "7fc4c149eaeefd2cf76f1e1b8ed62cb4",
+    "url": "/registration-form/static/media/Union.7fc4c149.svg"
+  },
+  {
+    "revision": "6258ce037f5ac3346077eea85d14984e",
+    "url": "/registration-form/index.html"
+  }
+];
